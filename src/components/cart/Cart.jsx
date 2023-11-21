5dc9a84057b6f5525679ce22fda8e912f30069a3
Chulbul Singh <cs845418@gmail.com>
2023-11-21 21:01:38 +0530

    now cart items becoming zero

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -112,21 +112,20 @@
 // }
 // export default Cart;
 
-
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import axios from "axios";
 function Cart({ cart, setCart }) {
-  console.log(localStorage.getItem('token'));
-  const [cartLength,setCartLength]=useState(cart.length);
+  console.log(localStorage.getItem("token"));
+  const [cartLength, setCartLength] = useState(cart.length);
   const [showModal, setShowModal] = useState(false);
   const [orderId, setOrderId] = useState(null);
   const [userId, setUserId] = useState(1);
-  const response=[];
+  const response = [];
   const [productId, setProductId] = useState(1);
   const [orderDate, setOrderDate] = useState("2023-11-13T07:19:26.090Z");
-  const [paymentMethod,setPaymetMethod] = useState("Credit Card")
-  const [orderStatus,setOrderStatus]  = useState("confirmed")
+  const [paymentMethod, setPaymetMethod] = useState("Credit Card");
+  const [orderStatus, setOrderStatus] = useState("confirmed");
   const [totalAmount, setTotalAmount] = useState(null);
   const [quantity, setQuantity] = useState(0);
   const handleIncrement = (index) => {
@@ -150,20 +149,22 @@ function Cart({ cart, setCart }) {
   const handlePayNow = async () => {
     if (totalPrice > 0) {
       try {
-        for(let i=0;i<cartLength;i++){
-          const response= await axios.post("http://localhost:5046/api/Orders", {
-          UserId: userId,
-          ProductId: cart[i].id,
-          Quantity: cart[i].quantity,
-          OrderDate: orderDate,
-          TotalAmount:cart[i].TotalAmount*cart[i].quantity,
-          PaymentMethod:paymentMethod,
-          OrderStatus:orderStatus
-         
-        });
- 
+        for (let i = 0; i < cartLength; i++) {
+          const response = await axios.post(
+            "http://localhost:5046/api/Orders",
+            {
+              UserId: userId,
+              ProductId: cart[i].id,
+              Quantity: cart[i].quantity,
+              OrderDate: orderDate,
+              TotalAmount: cart[i].TotalAmount * cart[i].quantity,
+              PaymentMethod: paymentMethod,
+              OrderStatus: orderStatus,
+            }
+          );
         }
-       
+        setCart([]);
+
         console.log("Order created successfully");
         setShowModal(true);
         setOrderId(response.data.OrderId);
@@ -172,10 +173,10 @@ function Cart({ cart, setCart }) {
         setOrderDate(response.data.OrderDate);
         setTotalAmount(response.data.TotalAmount);
         setQuantity(response.data.Quantity);
-        setPaymetMethod(response.data.PaymentMethod)
-        setOrderStatus(response.data.OrderStatus)
+        setPaymetMethod(response.data.PaymentMethod);
+        setOrderStatus(response.data.OrderStatus);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     } else {
       alert("Nothing is in your cart. Add something to your cart.");
@@ -186,26 +187,32 @@ function Cart({ cart, setCart }) {
       {cart.map((item, index) => (
         <motion.div
           key={item.id}
-          className="flex items-center justify-between w-full align-center space-y-6 "
+          className="flex items-center justify-between w-full p-4"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1 }}
         >
-          <h3 className="text-white font-bold text-xl ml-10">{item.name}</h3>
-          <p className="text-white font-semibold text-xl">Rs.{item.price}</p>
-          <button
-            onClick={() => handleIncrement(index)}
-            className=" bg-green-500 text-white px-2 py-1 rounded"
-          >
-            +
-          </button>
-          <p className="text-white text-xl ">{item.quantity}</p>
-          <button
-            onClick={() => handleDecrement(index)}
-            className="bg-red-500 text-white px-2 py-1 rounded mr-8"
-          >
-            -
-          </button>
+          <div className="flex items-center space-x-4">
+            <h3 className="text-white font-bold text-xl">{item.name}</h3>
+            <p className="text-white font-semibold text-xl ml-4">
+              Rs.{item.price}
+            </p>
+          </div>
+          <div className="flex items-center space-x-4">
+            <button
+              onClick={() => handleIncrement(index)}
+              className="bg-green-500 text-white px-2 py-1 rounded"
+            >
+              +
+            </button>
+            <p className="text-white text-xl">{item.quantity}</p>
+            <button
+              onClick={() => handleDecrement(index)}
+              className="bg-red-500 text-white px-2 py-1 rounded"
+            >
+              -
+            </button>
+          </div>
         </motion.div>
       ))}
       <h2 className="text-white mt-6 text-2xl">Total price: Rs.{totalPrice}</h2>
@@ -218,7 +225,7 @@ function Cart({ cart, setCart }) {
       {showModal && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-[#555b96] p-4 rounded shadow-lg w-[800px] h-[200px] items-center justify-center text-white">
-            <h2 className="text-2xl mb-4 text-center ">
+            <h2 className="text-2xl mb-4 text-center">
               Order Placed Successfully
             </h2>
             <p className="text-center">Thanks for shopping with us!</p>
